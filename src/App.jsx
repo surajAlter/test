13a442b39444c3b6d94fa6e373d3ae27f5a0b761
Suraj Sutradhar <suraj.sutradhar21b@iiitg.ac.in>
2024-07-02 14:30:57 +0530

    bg color picker done

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import './App.css'
 function App() {
   const [color, setColor] = useState("DodgerBlue")
   return (
-    <div id="container" className='w-screen h-screen flex flex-col items-center justify-end pb-4 text-slate-300'
-      style={{backgroundColor:color}}
+    <div id="container" className="w-screen h-screen flex flex-col justify-around items-center"
+    style={{backgroundColor:color}}
     >
-      <h1 className='text-2xl font-bold'>Select Color</h1>
-      <div className="bg-gray-300 px-4 py-2 rounded-full flex flex-wrap justify-between w-96 my-4 text-black  shadow-lg shadow-slate-400 text-white">
-        <button onClick={()=>setColor("red")} className='hover:ring-2 hover:ring-black hover:ring-offset-1 bg-red-700 rounded-lg  px-3 py-2'>Red</button>
-        <button onClick={()=>setColor("green")} className='hover:ring-2 hover:ring-black hover:ring-offset-1 bg-green-700 rounded-lg px-3 py-2'>Green</button>
-        <button onClick={()=>setColor("blue")} className='hover:ring-2 hover:ring-black hover:ring-offset-1 bg-blue-700 rounded-lg px-3 py-2'>Blue</button>
-        <button onClick={()=>setColor("black")} className='hover:ring-2 hover:ring-black hover:ring-offset-1 bg-black rounded-lg px-3 py-2'>Black</button>
+      <h1 className='text-4xl font-serif italic' style={{color:(color.charAt(1)>="E"?"black":"white")}}>This is a sample text</h1>
+
+      <div className='flex items-center space-x-2 my-4'>
+        
+        <h1 className='text-2xl font-bold' style={{color:(color.charAt(1)>="E"?"black":"white")}}>Select Color : </h1>
+
+        <input type="color" id="clr" value={color} onChange={(e)=>setColor(e.target.value)} className='w-8 h-6 border-none'/>
+
       </div>
     </div>
   )
