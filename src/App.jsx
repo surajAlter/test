9b3c228c96f708212e695f678a1b5e8ff3dbe20b
Suraj Sutradhar <suraj.sutradhar21b@iiitg.ac.in>
2024-07-06 21:13:25 +0530

    written by me

--- src/App.jsx
+++ src/App.jsx
@@ -2,82 +2,67 @@ import { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [minutes, setMinutes] = useState(25);
-  const [seconds, setSeconds] = useState(0);
-  const [start, setStart] = useState("START");
-  const [selectedMode, setSelectedMode] = useState("Pomodoro");
-  const [bgColor, setbgColor] = useState("bg-[#ba4949]");
-  const timerIdRef = useRef(null);
-  const alertShownRef = useRef(false);
+  //Constants
+  const pomo = 25, short = 5, long = 15, pomoclr = "[#ba4949]", shortclr = "teal-600", longclr = "cyan-700"
 
-  const decrementTime = () => {
-    setSeconds((prevSeconds) => {
-      if (prevSeconds > 0) {
-        return prevSeconds - 1;
-      } else {
-        if (minutes > 0) {
-          setMinutes((prevMinutes) => prevMinutes - 1);
-          return 59;
-        } else {
-          clearInterval(timerIdRef.current);
-          setStart("START");
-          if (!alertShownRef.current) {
-            alert('Time\'s up!');
-            alertShownRef.current = true;
-          }
-          return 0;
-        }
-      }
-    });
-  };
-
-  const setTimer = () => {
-    if (start === "STOP") {
-      setStart("START");
-      clearInterval(timerIdRef.current);
-    } else {
-      setStart("STOP");
-      alertShownRef.current = false;
-      decrementTime();
-      timerIdRef.current = setInterval(decrementTime, 1000);
-    }
-  };
-
-  const resetTimer = () => {
-    clearInterval(timerIdRef.current);
-    setStart("START");
-    if (selectedMode === "Pomodoro") {
-      setMinutes(25);
-      setSeconds(0);
-    } else if (selectedMode === "Short Break") {
-      setMinutes(5);
-      setSeconds(0);
-    } else if (selectedMode === "Long Break") {
-      setMinutes(15);
-      setSeconds(0);
-    }
-  };
-
-  const selectMode = (mode) => {
-    setSelectedMode(mode);
-    if (mode === "Pomodoro") {
-      setbgColor("bg-rose-500")
-      setMinutes(25)
-    } else if (mode === "Short Break") {
-      setbgColor("bg-teal-600")
-      setMinutes(5)
-    } else if (mode === "Long Break") {
-      setbgColor("bg-cyan-700")
-      setMinutes(15)
-    }
-    setSeconds(0);
-    clearInterval(timerIdRef.current);
-    setStart("START");
-  };
+  //For start/stop button
+  const [isRunning, setIsRunning] = useState(false)
+  const [seconds, setSeconds] = useState(0)
+  const [limit, setLimit] = useState(pomo)
+  let timer = useRef(null)
 
   useEffect(() => {
-    return () => clearInterval(timerIdRef.current);
-  }, []);
+    if (seconds >= limit) {
+      window.alert("Please reset the timer!");
+      setIsRunning(false);
+      return;
+    }
+
+    if (isRunning) {
+      timer.current = setInterval(() => {
+        setSeconds(prev => {
+          if (prev >= limit) {
+            clearInterval(timer.current);
+            let message = limit === pomo ? "Hooray! Looks like the time's up" : "Hello there! Looks like the time's up";
+            window.alert(message);
+            setIsRunning(false);
+
+            return prev
+          } else {
+            return prev + 1
+          }
+        })
+      }, 1000);
+    } else {
+      clearInterval(timer.current)
+    }
+
+    return () => clearInterval(timer.current)
+  }, [isRunning, limit])
+
+  //For mode change and reset
+  const [bgColor, setBgColor] = useState(`bg-${pomoclr}`)
+  const modeHandler = (mode = "") => {
+    //stop timer and set time back to 0
+    if (isRunning) setIsRunning(prev => !prev)
+    setSeconds(0)
+
+    //for reset
+    if (mode === "") return
+
+    //mode change
+    if (mode === pomo) {
+      setBgColor(`bg-${pomoclr}`)
+    } else if (mode === short) {
+      setBgColor(`bg-${shortclr}`)
+    } else {
+      setBgColor(`bg-${longclr}`)
+    }
+    setLimit(mode)
+  }
+
+  //formatting in timer string with leading zero if necessary
+  const format = (time) => (time <= 9) ? "0" + String(time) : String(time)
 
   return (
     <div
@@ -103,11 +88,13 @@ function App() {
           <button
             className='bg-white/20 backdrop-blur-sm rounded-md p-1'
           >
-            <div
+            <a
+              href='https://github.com/suraj-sutradhar0702'
               className='bg-violet-900 rounded-lg px-2'
+              target='__blank'
             >
               S
-            </div>
+            </a>
           </button>
         </div>
       </div>
@@ -118,20 +105,20 @@ function App() {
           className=''
         >
           <button
-            className={`rounded-md px-2 py-1 ${selectedMode === "Pomodoro" ? "bg-black/30 backdrop-blur-sm" : ""}`}
-            onClick={() => selectMode("Pomodoro")}
+            className={`rounded-md px-2 py-1 ${limit === pomo ? "bg-black/30 backdrop-blur-sm" : ""}`}
+            onClick={() => modeHandler(pomo)}
           >
             Pomodoro
           </button>
           <button
-            className={`rounded-md p-2 ${selectedMode === "Short Break" ? "bg-black/30 backdrop-blur-sm" : ""}`}
-            onClick={() => selectMode("Short Break")}
+            className={`rounded-md p-2 ${limit === short ? "bg-black/30 backdrop-blur-sm" : ""}`}
+            onClick={() => modeHandler(short)}
           >
             Short Break
           </button>
           <button
-            className={`rounded-md p-2 ${selectedMode === "Long Break" ? "bg-black/30 backdrop-blur-sm" : ""}`}
-            onClick={() => selectMode("Long Break")}
+            className={`rounded-md p-2 ${limit === long ? "bg-black/30 backdrop-blur-sm" : ""}`}
+            onClick={() => modeHandler(long)}
           >
             Long Break
           </button>
@@ -139,7 +126,7 @@ function App() {
         <div
           className='text-5xl md:text-8xl font-bold my-5'
         >
-          {(minutes < 10) ? `0${minutes}` : minutes}:{(seconds < 10) ? `0${seconds}` : seconds}
+          {format((limit - Math.ceil(seconds / 60)))}:{format((60 - (seconds % 60)) % 60)}
         </div>
 
         <div
@@ -147,14 +134,14 @@ function App() {
         >
           <button
             className='bg-white font-bold text-black text-2xl rounded-l-lg py-1 md:py-3 w-32 md:w-40'
-            onClick={setTimer}
+            onClick={() => setIsRunning((prev) => (!prev))}
           >
-            {start}
+            {isRunning ? "STOP" : "START"}
           </button>
 
           <button
             className='bg-white rounded-r-lg py-3 w-10 flex items-center justify-center border-l-2 border-gray-500'
-            onClick={resetTimer}
+            onClick={() => modeHandler()}
           >
             <img
               src="./reset.svg"
